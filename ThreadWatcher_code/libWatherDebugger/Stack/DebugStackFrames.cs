d9036c34e805fc4f1eb081ad3bb8b1c6dcfc136c
LockeChen <locke12456@gmail.com>
2014-05-21 14:08:20 +0800

    add stack frame list

---
+++ ThreadWatcher_code/libWatherDebugger/Stack/DebugStackFrames.cs
@@ -0,0 +1,25 @@
+﻿using libWatherDebugger.DocumentContext;
+using libWatherDebugger.Memory;
+using Microsoft.VisualStudio.Debugger.Interop;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace libWatherDebugger.Stack
+{
+    public class DebugStackFrames : List<DebugStackFrame>
+    {        
+        
+        public DebugStackFrames(DebugStackFrame current_stack) : base()
+        {
+            DebugStackFrameFactory factory = new DebugStackFrameFactory( current_stack.Thread );
+            AddRange(factory.ProductList);
+        }
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+}
