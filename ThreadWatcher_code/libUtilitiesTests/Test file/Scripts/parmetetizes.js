bb1fc3bdc632aa455b81cc586dba99afe50c39a1
locke12456 <locke12456@gmail.com>
2014-07-17 16:52:07 +0800

    update project

--- ThreadWatcher_code/libUtilitiesTests/Test file/Scripts/parmetetizes.js
+++
@@ -1,7 +0,0 @@
-
-function init(args){
-	for(var key in args)
-		console.log("key :" +key+ ", value : "+ args[key]);
-	return true;
-}
-result = init(args);
--- ThreadWatcher_code/libWatcherDialogTest/Test file/Scripts/bp1.js
+++
@@ -1,4 +0,0 @@
-var file = { filename: "ConsoleApplication1.cpp", line: 41 , type: "int"};
-var variable = addWatchponitFormAPi.BreakPointInfo(file);
-variable.AddCondition("*addd!=0&&*addd>998");
-var result = variable;
