0b1c51c0055e75ec519d287de4421952c0a43e57
locke12456 <locke12456@gmail.com>
2014-07-18 15:19:41 +0800

    add DebugDocumentException

---
+++ ThreadWatcher_code/libWatherDebugger/Exception/DebugItem/DebugDocumentException.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace libWatherDebugger.Exception.DebugItem
+{
+    public abstract class DebugDocumentException : ThreadDebuggerException
+    {
+        protected override string _type
+        {
+            get
+            {
+                return "DebugDocument";
+            }
+        }
+    }
+    public class GetCodeContextInfoFail : DebugDocumentException
+    {
+        protected override string _message
+        {
+            get
+            {
+                return "get code information fail .";
+            }
+        }
+    }
+}
