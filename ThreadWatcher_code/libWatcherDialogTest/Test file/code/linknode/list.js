d4eda726e4827f64268ed149002081722eba5636
locke12456 <locke12456@gmail.com>
2014-07-24 15:04:51 +0800

    clear

--- ThreadWatcher_code/libWatcherDialogTest/Test file/code/linknode/list.js
+++
@@ -1,5 +0,0 @@
-﻿var file = { filename: "linknode.cpp", line: 47, type: "Node" };
-var variable = addWatchponitFormAPi.BreakPointInfo(file);
-variable.AddCondition("node->counter>0");
-variable.SetWatchTarget({ name: "counter", type: "variable" });
-var result = variable;
